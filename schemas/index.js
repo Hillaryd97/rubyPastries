b9756f71d59876e8baf5f4004025b50acafcb1c3
Simi Dickson <Hillaryd97@gmail.com>
2023-09-17 17:24:35 +0100

    First Commit

--- schemas/index.js
+++ schemas/index.js
@@ -1,1 +1,5 @@
-export const schemaTypes = []
+import banner from './banner'
+import product from './product'
+import categories from './categories'
+
+export const schemaTypes = [banner, product, categories]
